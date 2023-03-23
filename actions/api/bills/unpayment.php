5ecdc6241897150b7df9418558d9cbc07ff9349a
Hamzah Fauzy <hamzahfauzy@Hamzahs-MacBook-Pro.local>
2023-03-24 05:39:05 +0700

    add subject to upayment bill api

--- actions/api/bills/unpayment.php
+++ actions/api/bills/unpayment.php
@@ -3,7 +3,7 @@
 $conn = conn();
 $db   = new Database($conn);
 
-$db->query = "SELECT bills.*, CONCAT(bills.bill_code,' - ',merchants.name) as bill_name, merchants.name, merchants.id as merchant_id FROM bills JOIN merchants ON merchants.id=bills.merchant_id WHERE bills.status='BELUM LUNAS'";
+$db->query = "SELECT bills.*, subjects.code subject_code, subjects.name subject_name, subjects.email subject_email, subjects.address subject_address, CONCAT(bills.bill_code,' - ',merchants.name) as bill_name, merchants.name, merchants.id as merchant_id FROM bills JOIN subjects ON subjects.id=bills.subject_id JOIN merchants ON merchants.id=bills.merchant_id WHERE bills.status='BELUM LUNAS'";
 $data  = $db->exec('all');
 
 echo json_encode([
