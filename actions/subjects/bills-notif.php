9412af03e7ef08ab2985ae6afcec4120ed59ba3d
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-10-24 12:25:44 +0700

    update bills notif message

--- actions/subjects/bills-notif.php
+++ actions/subjects/bills-notif.php
@@ -7,20 +7,28 @@ $subject = $db->single('subjects', ['id' => $_GET['id']]);
 
 $link = routeTo('subjects/bills-download', ['id' => $subject->id]);
 
-$message = "Yth. Bapak/Ibu Wali Murid 				
-$subject->name
+$message = "Yth. Ayah/Bunda Wali Murid 				
+[namasiswa]
 
-Semoga Bapak dan Ibu wali murid dari $subject->name senantiasa dalam keadaan sehat wal'afiat dan dalam perlindungan Allah SWT.
+Assalamualaikum Wr Wb.
 
-Mengingat akan dilaksanakanya Pengambilan Ijazah pada  13 November 2023, dengan ini disampaikan perincian tunggakan sampai pada bulan ini. Klik lampiran berikut:
+Semoga Ayah dan Bunda, wali murid dari ananda [namasiswa] senantiasa dalam keadaan sehat wal'afiat dan dalam perlindungan Allah SWT. Aamiin.
 
-$link
+Pada hari Senin, tanggal 13 November 2023, akan dilaksanakan sesi Pengambilan Ijazah siswa melalui wali kelas.
 
-Untuk itu, demi kelancaran proses belajar mengajar, dalam rangka memberikan pendidikan terbaik bagi putra-putri Bapak dan Ibu, kami mohon kerja samanya untuk melunasi tunggakan tersebut di atas. 
+Untuk sesi pengambilan ijazah ini, diharapkan semua administrasi siswa sudah terselesaikan. Bagi yang belum menyelesaikan administrasi siswa, mohon izin untuk menyampaikan perincian besarannya.
 
-Paling lambat tanggal 10 November 2023.
+Informasi besaran ini bisa didapatkan dengan cara klik link berikut:
 
-Demikian pemberitahuan kami. Atas perhatiannya, kami ucapkan terimakasih.
+[link]
+
+(SAVE PHONEBOOK nomer WA ini agar link bisa diklik).
+
+Mohon penyelesaian administrasi siswa tersebut bisa dilakukan paling lambat tanggal 10 November 2023 melalui bendahara sekolah. 
+
+Demikian pemberitahuan ini. Atas perhatiannya, kami ucapkan terimakasih.
+
+Wassalamu'alaikum Wr Wb.
 
 Kepala Sekolah,
 Mohamad Roji'i, M.Pd.
@@ -29,10 +37,13 @@ Mohamad Roji'i, M.Pd.
 ---
 NB: Apabila ada data yang tidak sesuai, mohon berkenan menghubungi bendahara sekolah, Ustadzah Farida di 085791305947";
 
+$message = str_replace('[namasiswa]', $subject->name, $message);
+$message = str_replace('[link]', $link, $message);
+
 try {
     //code...
-    // $phone = '6282369378823'; // 
-    $phone = $subject->phone;
+    $phone = '6282369378823'; // 
+    // $phone = $subject->phone;
     $wa = Whatsapp::send($phone, $message);
     set_flash_msg(['success'=>'Notifikasi berhasil dikirim']);
 } catch (\Throwable $th) {
