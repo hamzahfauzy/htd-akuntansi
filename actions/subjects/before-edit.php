e2201568b358b076e3fb2d95bddf3287236ae1c1
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-11-28 11:18:31 +0700

    before edit fix

--- actions/subjects/before-edit.php
+++ actions/subjects/before-edit.php
@@ -4,7 +4,7 @@ $_POST['group_id'] = $_POST[$table]['group'];
 
 unset($_POST[$table]['group']);
 
-if(empty($_POST[$table]['parent_id']))
+if(empty($_POST[$table]['parent_id']) || $_POST[$table]['parent_id'] == '')
 {
     unset($_POST[$table]['parent_id']);
 }
