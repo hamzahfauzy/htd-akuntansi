16ef688a50425e90e24146daecd4e3afc6d1abd1
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-12-22 03:09:16 +0700

    update notif

--- actions/subjects/bills-notif.php
+++ actions/subjects/bills-notif.php
@@ -14,19 +14,19 @@ Assalamu'alaikum Wr Wb.
 
 Semoga Ayah dan Bunda, wali murid dari ananda [namasiswa] senantiasa dalam keadaan sehat wal'afiat dan dalam perlindungan Allah SWT. Aamiin.
 
-Izin menginformasikan mengenai jadual  pembagian buku Semester 2 oleh Walikelas kepada Ananda, yang insyaAllah akan dilaksanakan pada hari Selasa, tanggal 02 Januari 2024.
+Izin menginformasikan mengenai jadual *pembagian buku Semester 2* oleh Walikelas kepada Ananda, yang insyaAllah akan dilaksanakan pada hari *Selasa, tanggal 02 Januari 2024*.
 
-Untuk kelancaran proses tersebut, dimohon semua administrasi siswa sudah terselesaikan. Bagi yang belum, mohon izin menyampaikan perincian besarannya dengan cara klik link berikut:
+Untuk kelancaran proses tersebut, dimohon *semua administrasi siswa sudah terselesaikan*. Bagi yang belum, mohon izin menyampaikan perincian besarannya dengan cara klik link berikut:
 
 [link]
 
 Penyelesaian administrasi siswa tersebut bisa dibayarkan paling lambat tanggal 30 Januari 2024.
 
-Pembayaran administrasi sekolah Ananda dilakukan melalui system' autodebet, yaitu dana disetorkan  ke rekening Ananda masing-masing, baik transfer ke rekening Ananda, ataupun menitipkan tabungan yang sudah tertulis slip menabungnya ke Bendahara sekolah (slip tabungan tersedia di meja Bendahara sekolah).*
+*Pembayaran administrasi sekolah Ananda* dilakukan melalui system autodebet, yaitu *dana disetorkan  ke rekening Ananda masing-masing, baik transfer ke rekening Ananda*, ataupun menitipkan *tabungan yang sudah tertulis slip menabungnya* ke Bendahara sekolah (slip tabungan tersedia di meja Bendahara sekolah).*
 
 Selama libur sekolah, Kantor Khazanah Ilmu buka hari Senin-Jum'at (08.00-12.00) dan hari Sabtu (08.00-12.00).
 
-Mohon berkenan juga untuk rutin melakukan pengecekan mutasi di rekening Ananda masing-masing, apakah sudah sesuai dengan dana yang Ayah/Bunda setorkan ke rekening Ananda dan juga pendebetan dari bank terkait administrasi sekolah.
+Mohon berkenan juga untuk rutin melakukan pengecekan mutasi di rekening Ananda masing-masing, apakah *sudah sesuai dengan dana yang Ayah/Bunda* setorkan ke rekening Ananda dan juga *pendebetan dari bank terkait administrasi sekolah*.
 
 Demikian pemberitahuan ini kami sampaikan. Atas perhatian dan kerjasamanya kami ucapkan terimakasih.
 
@@ -38,7 +38,7 @@ Mohamad Roji'i, M.Pd.
 
 
 
-SAVE nomer WA ini agar link bisa diklik dan mohon untuk memberikan konfirmasi dengan cara mereply pesan ini
+_SAVE nomer WA ini agar link bisa diklik dan mohon untuk memberikan konfirmasi dengan cara mereply pesan ini_
 ---
 
 
