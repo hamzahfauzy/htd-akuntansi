cb8fcd9475297ef7ab04fc2930c82ed956c63208
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-12-22 02:56:27 +0700

    update bill notif

--- actions/subjects/bills-notif.php
+++ actions/subjects/bills-notif.php
@@ -10,23 +10,26 @@ $link = routeTo('subjects/bills-download', ['id' => $subject->id]);
 $message = "Yth. Ayah/Bunda Wali Murid 				
 [namasiswa]
 
-Assalamualaikum Wr Wb.
+Assalamu'alaikum Wr Wb.
 
 Semoga Ayah dan Bunda, wali murid dari ananda [namasiswa] senantiasa dalam keadaan sehat wal'afiat dan dalam perlindungan Allah SWT. Aamiin.
 
-Pada hari Senin, tanggal 13 November 2023, akan dilaksanakan sesi Pengambilan Ijazah siswa melalui wali kelas.
+Izin menginformasikan mengenai jadual  pembagian buku Semester 2 oleh Walikelas kepada Ananda, yang insyaAllah akan dilaksanakan pada hari Selasa, tanggal 02 Januari 2024.
 
-Untuk sesi pengambilan ijazah ini, diharapkan semua administrasi siswa sudah terselesaikan. Bagi yang belum menyelesaikan administrasi siswa, mohon izin untuk menyampaikan perincian besarannya.
-
-Informasi besaran ini bisa didapatkan dengan cara klik link berikut:
+Untuk kelancaran proses tersebut, dimohon semua administrasi siswa sudah terselesaikan. Bagi yang belum, mohon izin menyampaikan perincian besarannya dengan cara klik link berikut:
 
 [link]
 
-*_SAVE nomer WA ini agar link bisa diklik_
+Penyelesaian administrasi siswa tersebut bisa dibayarkan paling lambat tanggal 30 Januari 2024.
 
-Mohon penyelesaian administrasi siswa tersebut bisa dilakukan paling lambat tanggal 10 November 2023 melalui bendahara sekolah. 
+Pembayaran administrasi sekolah Ananda dilakukan melalui system' autodebet, yaitu dana disetorkan  ke rekening Ananda masing-masing, baik transfer ke rekening Ananda, ataupun menitipkan tabungan yang sudah tertulis slip menabungnya ke Bendahara sekolah (slip tabungan tersedia di meja Bendahara sekolah).*
 
-Demikian pemberitahuan ini. Atas perhatiannya, kami ucapkan terimakasih.
+Selama libur sekolah, Kantor Khazanah Ilmu buka hari Senin-Jum'at (08.00-12.00) dan hari Sabtu (08.00-12.00).
+
+Mohon berkenan juga untuk rutin melakukan pengecekan mutasi di rekening Ananda masing-masing, apakah sudah sesuai dengan dana yang Ayah/Bunda setorkan ke rekening Ananda dan juga pendebetan dari bank terkait administrasi sekolah.
+
+Demikian pemberitahuan ini kami sampaikan. Atas perhatian dan kerjasamanya kami ucapkan terimakasih.
+
 
 Wassalamu'alaikum Wr Wb.
 
@@ -34,8 +37,12 @@ Kepala Sekolah,
 Mohamad Roji'i, M.Pd.
 
 
+
+SAVE nomer WA ini agar link bisa diklik dan mohon untuk memberikan konfirmasi dengan cara mereply pesan ini
 ---
-NB: Apabila ada kendala atau data yang tidak sesuai, mohon berkenan menghubungi bendahara sekolah, Ustadzah Farida di 085791305947.";
+
+
+NB : Jika ada yang belum sesuai, bisa melakukan konfirmasi langsung ke Bendahara sekolah, Ustadzah Farida di 085791305947.";
 
 $message = str_replace('[namasiswa]', $subject->name, $message);
 $message = str_replace('[link]', $link, $message);
