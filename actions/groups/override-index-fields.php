cbfc08385afbd3070d6625bdaa5fa11db6f6d18b
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-01-08 10:20:42 +0700

    add jumlah siswa

---
+++ actions/groups/override-index-fields.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'parent_id' => [
+        'label' => 'Parent',
+        'type'  => 'options-obj:groups,id,name'
+    ],
+    'name' => [
+        'label' => 'Nama Group',
+        'type'  => 'text'
+    ],
+    'jumlah_siswa' => [
+        'label' => 'Jumlah Siswa',
+        'type'  => 'number'
+    ],
+];
