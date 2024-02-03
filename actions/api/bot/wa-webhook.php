c06b599463a3bfcf0c5db39cf3365b70ef54ea3c
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-02-03 08:36:51 +0700

    fix message

--- actions/api/bot/wa-webhook.php
+++ actions/api/bot/wa-webhook.php
@@ -54,19 +54,20 @@ try {
         $parent = explode('QQ', $data->address);
         $parent_name = $parent[0];
         $msg = "Assalamualaikum Wr Wb.
-        Terima kasih telah mengunjungi Sekolah Khazanah Ilmu, kami harap Bapak/Ibu $parent_name dalam keadaan sehat selalu.
+Terima kasih telah mengunjungi Sekolah Khazanah Ilmu, kami harap Bapak/Ibu $parent_name dalam keadaan sehat selalu.
         
-        Silakan pilih informasi yang dibutuhkan: (ketik angkanya saja). Beberapa pilihan hanya bisa diakses siswa aktif.
+Silakan pilih informasi yang dibutuhkan: (ketik angkanya saja). Beberapa pilihan hanya bisa diakses siswa aktif.
         
-        1. Cek Status Pembayaran (privat)
-        2. Info Jadwal/Kegiatan terdekat
-        3. Info PPDB
-        4. Cek status PPDB
-        5. Cek kelulusan (privat)
-        6. Download Brosur
-        Silahkan sebarkan informasi ini untuk saudara, keluarga & lingkungan terdekat Bapak/Ibu $parent_name 🙏
+1. Cek Status Pembayaran (privat)
+2. Info Jadwal/Kegiatan terdekat
+3. Info PPDB
+4. Cek status PPDB
+5. Cek kelulusan (privat)
+6. Download Brosur
+
+Silahkan sebarkan informasi ini untuk saudara, keluarga & lingkungan terdekat Bapak/Ibu $parent_name 🙏
         
-        Agar semakin banyak yang mendapatkan manfaat atas keberadaan sekolah Khazanah Ilmu di tengah masyarakat";
+Agar semakin banyak yang mendapatkan manfaat atas keberadaan sekolah Khazanah Ilmu di tengah masyarakat";
         Whatsapp::send($froms[0], $msg);
         die();
     }
